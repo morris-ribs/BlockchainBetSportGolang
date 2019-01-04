67eaf7e2364871a286df9b42dd93dede48b15dfb
morris-ribs <maumribeiro@gmail.com>
2019-01-04 09:21:18 +0100

    Create Genesis Block

--- bet/blockchain.go
+++ bet/blockchain.go
@@ -1,18 +1,13 @@
 package bet
 
+import "time"
+
 //RegisterBet registers a bet in our blockchain
 func (b *Blockchain) RegisterBet(bet Bet) bool {
 	b.PendingBets = append(b.PendingBets, bet)
 	return true
 }
 
-/*    const newNodeUrl = req.body.newNodeUrl;
-    const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1;
-    const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl;
-    if (nodeNotAlreadyPresent && notCurrentNode) {
-        bitcoin.networkNodes.push(newNodeUrl);
-    }
-	res.json({ note: "New node registered successfully." });*/
 func contains(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
@@ -29,3 +24,18 @@ func (b *Blockchain) RegisterNode(node string) bool {
 	}
 	return true
 }
+
+//CreateNewBlock ...
+func (b *Blockchain) CreateNewBlock(nonce int32, previousBlockHash string, hash string) Block {
+	newBlock := Block{
+		Bets:      b.PendingBets,
+		Timestamp: time.Now(),
+		Nonce:     nonce,
+		Hash:      hash, PreviousBlockHash: previousBlockHash}
+
+	b.PendingBets = Bets{}
+	b.Chain = append(b.Chain, newBlock)
+	return newBlock
+}
+
+//GetLastBlock ...
--- bet/router.go
+++ bet/router.go
@@ -62,7 +62,7 @@ var routes = Routes{
 		controller.RegisterBet,
 	},
 	Route{
-		"RegisterBet",
+		"RegisterAndBroadcastBet",
 		"POST",
 		"/bet/broadcast",
 		controller.RegisterAndBroadcastBet,
@@ -79,6 +79,10 @@ var routes = Routes{
 func NewRouter(nodeAddress string) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	controller.currentNodeURL = "http://localhost:" + nodeAddress
+
+	// create Genesis block
+	controller.blockchain.CreateNewBlock(100, "0", "0")
+
 	for _, route := range routes {
 		var handler http.Handler
 		handler = route.HandlerFunc
