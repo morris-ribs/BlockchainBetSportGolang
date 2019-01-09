8d3dc351333a9649884991941712eb49b195f71b
morris-ribs <maumribeiro@gmail.com>
2019-01-09 10:01:16 +0100

    modification of return values

--- bet/model.go
+++ bet/model.go
@@ -2,10 +2,10 @@ package bet
 
 //Bet represents a bet
 type Bet struct {
-	PlayerName   string `json:"playerName"`
-	MatchID      string `json:"matchId"`
-	TeamOneScore int32  `json:"teamOneScore"`
-	TeamTwoScore int32  `json:"teamTwoScore"`
+	PlayerName   string `json:"player_name"`
+	MatchID      string `json:"match_id"`
+	TeamOneScore int32  `json:"team_one_score"`
+	TeamTwoScore int32  `json:"team_two_score"`
 }
 
 //Bets is an array of Bet
@@ -13,12 +13,12 @@ type Bets []Bet
 
 //Block ...
 type Block struct {
-	Index             int
-	Timestamp         int64
-	Bets              Bets
-	Nonce             int
-	Hash              string
-	PreviousBlockHash string
+	Index             int    `json:"index"`
+	Timestamp         int64  `json:"timestamp"`
+	Bets              Bets   `json:"bets"`
+	Nonce             int    `json:"nonce"`
+	Hash              string `json:"hash"`
+	PreviousBlockHash string `json:"previous_block_hash"`
 }
 
 //Blocks is an array of Block
@@ -26,9 +26,9 @@ type Blocks []Block
 
 //Blockchain ...
 type Blockchain struct {
-	Chain        Blocks
-	PendingBets  Bets
-	NetworkNodes []string
+	Chain        Blocks   `json:"chain"`
+	PendingBets  Bets     `json:"pending_bets"`
+	NetworkNodes []string `json:"network_nodes"`
 }
 
 //BlockData is used in hash calculations
